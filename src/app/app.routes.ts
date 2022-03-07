b540c3aa81074d788637db897dded95b8ea25e85
Soraya Sanchez <sorayasanchez0503@gmail.com>
2022-03-07 08:58:48 -0500

    segundo commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import {RouterModule, Routes} from '@angular/router'; 
 import { Component } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+//import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+
+
 
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent }, 
+  { path: 'about', component: AboutComponent }, 
+  { path: 'heroes', component: HeroesComponent }, 
+  { path: 'heroe/:id', component: HeroeComponent },
+  { path: 'buscar/:termino', component: BuscadorComponent }, 
+ // { path: 'HeroeTarjetaComponent', component: HeroeTarjetaComponent }, 
   { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
